6655f0d479f81f1695b4c492baa430a8380fedd1
Joshua Gomez <joshuagomez@library.ucla.edu>
2019-08-14 13:09:33 -0700

    Registered all models with Django admin. Closes TRRA-33

--- terra/admin.py
+++ terra/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Unit, Employee, TravelRequest, Activity, Vacation, Fund, \
+	Approval, EstimatedExpense, ActualExpense
 
-# Register your models here.
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(TravelRequest)
+class TravelRequestAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Vacation)
+class VacationAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Fund)
+class FundAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Approval)
+class ApprovalAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(EstimatedExpense)
+class EstimatedExpenseAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(ActualExpense)
+class ActualExpenseAdmin(admin.ModelAdmin):
+	pass
