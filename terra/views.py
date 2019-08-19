8e1a0990cafcd0a555bb4d1203e8c50b9c4e2c2a
Ashton Prigge <ashton.prigge@gmail.com>
2019-08-19 11:31:53 -0700

    Removed an employee detail view

--- terra/views.py
+++ terra/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
 from .models import Employee
 from django.views.generic import DetailView
-from django.contrib.auth import views as auth_views
-
-class EmployeeDetailView(DetailView):
-	model = Employee
-
-
-
 
 # Create your views here.
