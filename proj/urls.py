c857059fc84e1c761a011e584a4b253b2dd42151
Ashton Prigge <ashton.prigge@gmail.com>
2019-08-21 09:30:35 -0700

    Fixed styling. Fixed password reset and password change urls.

--- proj/urls.py
+++ proj/urls.py
@@ -22,11 +22,24 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='terra/registration/login.html')),
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='terra/registration/logout.html')),
-    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(template_name='terra/registration/password_change.html')),
-    path('accounts/password_change_done/', auth_views.PasswordChangeDoneView.as_view(template_name='terra/registration/password_change_done.html')),
-    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='terra/registration/password_reset.html')),
-    path('accounts/password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='terra/registration/password_reset_done.html')),
-    path('accounts/password_reset_confirm/', auth_views.PasswordResetConfirmView.as_view(template_name='terra/registration/password_reset_confirm.html')),
-    path('accounts/password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='terra/registration/password_reset_complete.html')),
-    
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(
+        template_name='terra/registration/password_change.html'), 
+        name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='terra/registration/password_change_done.html'),
+        name='password_change_done'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='terra/registration/password_reset.html'),
+        name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='terra/registration/password_reset_done.html'),
+        name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='terra/registration/password_reset_confirm.html'),
+        name='password_reset_confirm'),
+    path('accounts/password/done', 
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='terra/registration/password_reset_complete.html'),
+        name='password_reset_complete'),
 ]
