fbddf244c09558bf560446153ca46c01756a5843
Ashton Prigge <ashton.prigge@gmail.com>
2019-08-27 11:35:31 -0700

    Added dashboard path and begining view and template.

--- terra/views.py
+++ terra/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import DetailView, ListView
 
 from .models import (
     Unit,
