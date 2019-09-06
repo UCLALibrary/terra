ccd9876dd2486f381fd90c5a58c398cd32e8a8b0
David Rickard <drickard1967@library.ucla.edu>
2019-09-06 09:07:29 -0700

    TRRA-63 added admin filters
    Added admin filters (best-guess on desired fields) to each model.
    Default right-side placement of the filter list.
    Included "treq" field in some filters (e.g., Vacation) but it doesn't show in filter list--not certain if this is from lack of data or field type...

--- terra/admin.py
+++ terra/admin.py
@@ -15,13 +15,13 @@ from .models import (
 @admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
     list_display = ("name", "manager", "employee_count", "parent_unit")
-
+    list_filter = ("name","manager","parent_unit")
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ("uid", "name", "unit", "supervisor", "active")
     list_display_links = ("uid", "name")
-
+    list_filter = ("user","unit","active","uid","supervisor",)
 
 @admin.register(TravelRequest)
 class TravelRequestAdmin(admin.ModelAdmin):
@@ -34,33 +34,35 @@ class TravelRequestAdmin(admin.ModelAdmin):
         "funded",
         "closed",
     )
-
+    list_filter = ("traveler","activity","departure_date","return_date","days_ooo","closed","funding",)
 
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ("name", "start", "end", "city", "state", "country")
-
+    list_filter = ("name","start","end","city","state","country",)
 
 @admin.register(Vacation)
 class VacationAdmin(admin.ModelAdmin):
     list_display = ("id", "treq", "start", "end")
-
+    list_filter = ("treq","start","end",)
 
 @admin.register(Fund)
 class FundAdmin(admin.ModelAdmin):
     list_display = ("__str__", "manager")
-
+    list_filter = ("account","cost_center","fund","manager",)
 
 @admin.register(Approval)
 class ApprovalAdmin(admin.ModelAdmin):
     list_display = ("id", "treq", "type", "approver", "timestamp")
-
+    list_filter = ("timestamp","approver","treq","type",)
 
 @admin.register(EstimatedExpense)
 class EstimatedExpenseAdmin(admin.ModelAdmin):
     list_display = ("id", "treq", "type", "total_dollars")
-
+    list_filter = ("treq","type","total",)
 
 @admin.register(ActualExpense)
 class ActualExpenseAdmin(admin.ModelAdmin):
     list_display = ("id", "treq", "type", "total_dollars")
+    list_filter = ("treq","type","total","fund",)
+    
