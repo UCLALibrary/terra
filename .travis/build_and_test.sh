1c9e71ee500d3a36fefb914864ad0819f4dd6098
sgurnick <sgurnick@library.ucla.edu>
2019-09-17 09:32:47 -0700

    Modify to use travis-specific docker compose file

--- .travis/build_and_test.sh
+++ .travis/build_and_test.sh
@@ -1,14 +1,20 @@
-#!/bin/sh
-# Try doing it all in one script since Travis is too non-deterministic.....
+#!/bin/bash
 
-docker-compose up --build -d
-docker-compose exec django pip install -q coveralls --user --no-warn-script-location
+# Script to build and test the Django Terra app container
+
+DOCKER_COMPOSE_TRAVIS="docker-compose -f .travis/docker-compose_travis_test.yml"
+
+# Build the Django Terra App and Database images and start the containers
+${DOCKER_COMPOSE_TRAVIS} up --build -d
+
+# Install Converalls into the Django Terra container
+${DOCKER_COMPOSE_TRAVIS} exec django pip install -q coveralls --user --no-warn-script-location
 
 # Even though the build/up step above waits until the db is available, that's not always the case
 # Keep trying to set db permissions for the django test user, so it can create a clean db for testing.
-until docker-compose exec db mysql -u root -pthis-is-a-fake-password -e "grant all privileges on test_terra.* to 'terrauser'@'%';"; do
+until ${DOCKER_COMPOSE_TRAVIS} exec db mysql -u root -pthis-is-a-fake-password -e "grant all privileges on test_terra.* to 'terrauser'@'%';"; do
   echo "Waiting for mysql to become available..."
-  sleep 3 
+  sleep 3
 done
 
 # When building in Travis, the host directory is owned by Travis.  The volume mount in the django Docker
@@ -24,5 +30,4 @@ if [ "$TRAVIS" = "true" ]; then
 fi
 
 # Run the tests
-docker-compose exec django /home/django/.local/bin/coverage run --source=terra manage.py test terra
-
+${DOCKER_COMPOSE_TRAVIS} exec django /home/django/.local/bin/coverage run --source=terra manage.py test terra
