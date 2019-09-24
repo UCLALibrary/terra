b5a455c0c4b50b60dcb3fdec910d05e9de5c6447
drickard <drickard1967@library.ucla.edu>
2019-09-24 14:29:12 -0700

    TRRA-79: second try on fancy filters

--- terra/admin.py
+++ terra/admin.py
@@ -10,25 +10,43 @@ from .models import (
     EstimatedExpense,
     ActualExpense,
 )
-
-def custom_titled_filter(title):
-    class Wrapper(admin.FieldListFilter):
-        def __new__(cls, *args, **kwargs):
-            instance = admin.FieldListFilter.create(*args, **kwargs)
-            instance.title = title
-            return instance
-    return Wrapper
+from .filters import (
+   custom_titled_filter,
+   InputFilter,
+   UIDFilter,
+   EmpLastNameFilter,
+   UnitFilter,
+   SupervisorFilter,
+   ActivityNameFilter,
+   ActivityCityFilter,
+   ActivityStateFilter,
+   ActivityCountryFilter,
+   TreqEmpLastNameFilter,
+   TreqActivityNameFilter,
+   FundManagerFilter,
+   FundFilter,
+   AccountFilter,
+   CostCenterFilter,
+   ApproverNameFilter,
+)
 
 @admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
     list_display = ("name", "manager", "employee_count", "parent_unit")
     list_filter = ("name","manager",("parent_unit", custom_titled_filter('parent unit')),)
+    search_fields = ['name','parent_unit__name']
+    autocomplete_fields = ['manager','parent_unit']
+    def get_ordering(self, request):
+        return ['name']
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ("uid", "name", "unit", "supervisor", "active")
     list_display_links = ("uid", "name")
-    list_filter = ("user","unit","active","uid","supervisor",)
+    list_filter = (EmpLastNameFilter,UnitFilter,"active",UIDFilter,SupervisorFilter,) #"unit" "user" "uid" "supervisor"
+    search_fields = ['user__last_name__startswith','supervisor__user__last_name__startswith','unit__name']
+    autocomplete_fields = ['unit','supervisor']
+    sortable_by = ("uid", "name", "unit", "supervisor", "active")
 
 @admin.register(TravelRequest)
 class TravelRequestAdmin(admin.ModelAdmin):
@@ -45,35 +63,44 @@ class TravelRequestAdmin(admin.ModelAdmin):
         "allocations_total",
         "expenditures_total",
     )
-    list_filter = ("traveler","activity",("departure_date", custom_titled_filter('departure date')),("return_date", custom_titled_filter('return date')),("days_ooo", custom_titled_filter('days out-of-office')),"closed","funding",)
+    list_filter = (TreqEmpLastNameFilter,TreqActivityNameFilter,("departure_date", custom_titled_filter('Departure Date')),("return_date", custom_titled_filter('Return Date')),("days_ooo", custom_titled_filter('Days Out-of-Office')),"closed","funding",) # "traveler" "activity"
+    autocomplete_fields = ['funding']
+
 
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ("name", "start", "end", "city", "state", "country")
-    list_filter = ("name",("start", custom_titled_filter('start date')),("end", custom_titled_filter('end date')),"city","state","country",)
+    list_filter = (ActivityNameFilter,("start", custom_titled_filter('Start Date')),("end", custom_titled_filter('End Date')),ActivityCityFilter,ActivityStateFilter,ActivityCountryFilter,) #"name" "city" "state" "country"
+
 
 @admin.register(Vacation)
 class VacationAdmin(admin.ModelAdmin):
     list_display = ("id", "treq", "start", "end")
-    list_filter = ("treq__traveler",("treq__activity__name", custom_titled_filter('activity')),("start", custom_titled_filter('start date')),("end", custom_titled_filter('end date')),)
+    list_filter = (TreqEmpLastNameFilter,TreqActivityNameFilter,("start", custom_titled_filter('Start Date')),("end", custom_titled_filter('End Date')),) #"treq__traveler" ("treq__activity__name", custom_titled_filter('activity'))
+
 
 @admin.register(Fund)
 class FundAdmin(admin.ModelAdmin):
     list_display = ("__str__", "manager")
-    list_filter = ("account",("cost_center", custom_titled_filter('cost center')),"fund","manager",)
+    list_filter = (AccountFilter,CostCenterFilter,FundFilter,FundManagerFilter,) # "manager" "fund" "account" ("cost_center", custom_titled_filter('Cost Center'))
+    search_fields = ['fund']
+
 
 @admin.register(Approval)
 class ApprovalAdmin(admin.ModelAdmin):
     list_display = ("id", "treq", "type", "approver", "timestamp")
-    list_filter = (("timestamp", custom_titled_filter('approval date')),"approver","treq__traveler",("treq__activity__name", custom_titled_filter('activity')),"type",)
+    list_filter = (("timestamp", custom_titled_filter('Approval Date')),ApproverNameFilter,TreqEmpLastNameFilter,TreqActivityNameFilter,"type",) # "treq__traveler" ("treq__activity__name", custom_titled_filter('activity')) "approver"
+
 
 @admin.register(EstimatedExpense)
 class EstimatedExpenseAdmin(admin.ModelAdmin):
     list_display = ("id", "treq", "type", "total_dollars")
-    list_filter = ("treq__traveler",("treq__activity__name", custom_titled_filter('activity')),"type",("total", custom_titled_filter('total cost')),)
+    list_filter = (TreqEmpLastNameFilter,TreqActivityNameFilter,"type",("total", custom_titled_filter('Total Cost')),) # "treq__traveler" ("treq__activity__name", custom_titled_filter('activity'))
+
 
 @admin.register(ActualExpense)
 class ActualExpenseAdmin(admin.ModelAdmin):
     list_display = ("id", "treq", "type", "total_dollars")
-    list_filter = ("treq__traveler",("treq__activity__name", custom_titled_filter('activity')),"type",("total", custom_titled_filter('total cost')),"fund",)
+    list_filter = (TreqEmpLastNameFilter,TreqActivityNameFilter,"type",("total", custom_titled_filter('Total Cost')),"fund",) # "treq__traveler" ("treq__activity__name", custom_titled_filter('activity')) 
+    autocomplete_fields = ['fund']
  
---
+++ terra/filters.py
@@ -0,0 +1,208 @@
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+def custom_titled_filter(title):
+    class Wrapper(admin.FieldListFilter):
+        def __new__(cls, *args, **kwargs):
+            instance = admin.FieldListFilter.create(*args, **kwargs)
+            instance.title = title
+            return instance
+    return Wrapper
+
+class InputFilter(admin.SimpleListFilter):
+    template = 'terra/admin_filter.html'
+
+    def lookups(self, request, model_admin):
+        # Dummy, required to show the filter.
+        return ((),)
+
+    def choices(self, changelist):
+        # Grab only the "all" option.
+        all_choice = next(super().choices(changelist))
+        all_choice['query_parts'] = (
+            (k, v)
+            for k, v in changelist.get_filters_params().items()
+            if k != self.parameter_name
+        )
+        yield all_choice
+
+class UIDFilter(InputFilter):
+    parameter_name = 'uid'
+    title = _('University ID')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            uid = self.value()
+
+            return queryset.filter(
+                uid=uid
+            )
+
+class EmpLastNameFilter(InputFilter):
+    parameter_name = 'user__last_name'
+    title = _('Library Employee Last Name')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            user = self.value()
+
+            return queryset.filter(
+                user__last_name=user 
+            )
+
+class ApproverNameFilter(InputFilter):
+    parameter_name = 'approver__user__last_name'
+    title = _('Approver Last Name')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            user = self.value()
+
+            return queryset.filter(
+                approver__user__last_name=user 
+            )
+
+class FundManagerFilter(InputFilter):
+    parameter_name = 'manager__user__last_name'
+    title = _('Fund Manager Last Name')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            user = self.value()
+
+            return queryset.filter(
+                manager__user__last_name=user 
+            )
+
+class FundFilter(InputFilter):
+    parameter_name = 'fund'
+    title = _('Fund')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            fund = self.value()
+
+            return queryset.filter(
+                fund=fund 
+            )
+
+class CostCenterFilter(InputFilter):
+    parameter_name = 'cost_center'
+    title = _('Cost Center')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            cost_center = self.value()
+
+            return queryset.filter(
+                cost_center=cost_center 
+            )
+
+class AccountFilter(InputFilter):
+    parameter_name = 'account'
+    title = _('Account')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            account = self.value()
+
+            return queryset.filter(
+                account=account 
+            )
+
+class TreqEmpLastNameFilter(InputFilter):
+    parameter_name = 'treq__traveler__user__last_name'
+    title = _('Traveler Last Name')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            user = self.value()
+
+            return queryset.filter(
+                treq__traveler__user__last_name=user 
+            )
+
+class UnitFilter(InputFilter):
+    parameter_name = 'unit__name'
+    title = _('Library Unit')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            unit = self.value()
+
+            return queryset.filter(
+                unit__name=unit 
+            )
+            
+class SupervisorFilter(InputFilter):
+    parameter_name = 'supervisor__user__last_name'
+    title = _('Supervisor''s Last Name')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            user = self.value()
+
+            return queryset.filter(
+                supervisor__user__last_name=user 
+            )
+            
+class ActivityNameFilter(InputFilter):
+    parameter_name = 'name'
+    title = _('Name of Activity')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            name = self.value()
+
+            return queryset.filter(
+                name=name
+            )
+            
+class TreqActivityNameFilter(InputFilter):
+    parameter_name = 'treq__activity__name'
+    title = _('Name of Activity')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            name = self.value()
+
+            return queryset.filter(
+                treq__activity__name=name
+            )
+            
+class ActivityCityFilter(InputFilter):
+    parameter_name = 'city'
+    title = _('City')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            city = self.value()
+
+            return queryset.filter(
+                city=city
+            )
+            
+class ActivityStateFilter(InputFilter):
+    parameter_name = 'state'
+    title = _('State')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            state = self.value()
+
+            return queryset.filter(
+                state=state
+            )
+            
+class ActivityCountryFilter(InputFilter):
+    parameter_name = 'country'
+    title = _('Country')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            country = self.value()
+
+            return queryset.filter(
+                country=country
+            )
+            
