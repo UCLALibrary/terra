ac0ea75ce131b500676c39b42f9bfe3bd44480d0
drickard <drickard1967@library.ucla.edu>
2019-09-26 13:12:01 -0700

    TRRA-79 missed merge from MASTER views.py

--- terra/views.py
+++ terra/views.py
@@ -1,5 +1,9 @@
 from django.views.generic.list import ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.list import ListView
+from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import TravelRequest, Unit
+from .reports import unit_report
 
 from django.shortcuts import render, render_to_response
 from django.views.generic import CreateView
@@ -23,6 +27,43 @@ class UserDashboard(LoginRequiredMixin, ListView):
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(traveler__user=self.request.user)
+
+class UnitDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+
+    model = Unit
+    context_object_name = "unit"
+    login_url = "/accounts/login/"
+    redirect_field_name = "next"
+
+    def test_func(self):
+        if self.request.user.is_superuser:
+            return True
+        unit = self.get_object()
+        return self.request.user.employee in unit.super_managers()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["report"] = unit_report(self.object)
+        return context
+
+
+class UnitListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+
+    model = Unit
+    context_object_name = "units"
+    login_url = "/accounts/login/"
+    redirect_field_name = "next"
+
+    def test_func(self):
+        return (
+            self.request.user.is_superuser
+            or len(self.request.user.employee.managed_units.all()) > 0
+        )
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Unit.objects.filter(type="1")
+        return Unit.objects.filter(manager=self.request.user.employee)
 
 class AutocompleteJsonView(Base):
     """Overriding django admin's AutocompleteJsonView"""
