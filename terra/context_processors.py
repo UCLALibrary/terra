5ade9092a75f79db0fdf32c26f3066edf41e2ffd
Ashton Prigge <ashton.prigge@gmail.com>
2020-02-10 13:59:42 -0800

    TRRA-186: imported fy integer from utils in context processor.

--- terra/context_processors.py
+++ terra/context_processors.py
@@ -1,21 +1,4 @@
-from datetime import date
-from django.conf import settings
-
-import fiscalyear as FY
-import locale
-
-
-FY.START_MONTH = 7
-
-
-def current_fiscal_year(today=None):
-    today = date.today() if today is None else today
-    return FY.FiscalDate(today.year, today.month, today.day).fiscal_year
-
-
-def current_fiscal_year_int(today=None):
-    year = current_fiscal_year(today=today)
-    return year
+from .utils import current_fiscal_year_int
 
 
 def add_variable_to_context(request):
