a0e3fc60487f3b767e231f584a8d17d6d42e457d
Andy Kohler <akohler@library.ucla.edu>
2023-12-19 16:42:24 -0900

    Upgrade crispy forms and bootstrap

--- proj/settings.py
+++ proj/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "terra",
     "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
-CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
